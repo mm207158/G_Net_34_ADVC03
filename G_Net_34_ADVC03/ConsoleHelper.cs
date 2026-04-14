a2902663d6bb4d8cc92a067c18168f00ceb67c1c
mm207158 <divmariam2021@gmail.com>
2026-04-15 01:49:38 +0200

    commit 01

---
+++ G_Net_34_ADVC03/ConsoleHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_34_ADVC03
+{
+    internal class ConsoleHelper
+    {
+        public static void PrintList<T>(string listName, List<T> items)
+        {
+            Console.WriteLine($"{listName} : [{string.Join(", ", items)}]");
+
+        }
+
+        public static void PrintHashSet<T>(string setName, HashSet<T> items)
+        {
+            Console.WriteLine($"{setName} : [{string.Join(", ", items)}]");
+
+        }
+        public static void PrintQueue<T>(string queueName, Queue<T> items)
+        {
+            Console.WriteLine($"{queueName} : [{string.Join(", ", items)}]");
+
+        }
+        public static void PrintStack<T>(string stackName, Stack<T> items)
+        {
+            Console.WriteLine($"{stackName} : [{string.Join(", ", items)}]");
+
+        }
+        public static void PrintArray<T>(string arrayName, T[] items)
+        {
+            Console.WriteLine($"{arrayName} : [{string.Join(", ", items)}]");
+
+        }
+        public static void PrintSpan<T>(string name, Span<T> span)
+        {
+            Console.Write($"{name} : [");
+            foreach (var item in span)
+                Console.Write($"{item}, ");
+            Console.WriteLine("]");
+        }
+        public static void PrintCollection<T>(string colName, IEnumerable<T> items)
+        {
+            Console.WriteLine($"{colName} : [{string.Join(", ", items)}]");
+
+        }
+    }
+}
--- G_Net_34_ADVC03/Program.cs
+++ G_Net_34_ADVC03/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace G_Net_34_ADVC03
+﻿using System.ComponentModel;
+
+namespace G_Net_34_ADVC03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Exercise 1: Student Grade Manager
+            #region Q1
+            //List<int> grades = new List<int>() { 85, 92, 78, 95, 88, 70, 100, 65 };
+            #endregion
+            #region Q2 
+
+            //Console.WriteLine($"Count is : {grades.Count}");
+            //Console.WriteLine($"Frist Grade is : {grades[0]}");
+            //Console.WriteLine($"Last Grade is : {grades[^1]}"); 
+            #endregion
+            #region Q3
+            //ConsoleHelper.PrintList("Grades Before Sorting", grades);
+            //grades.Sort();
+            //ConsoleHelper.PrintList("Grades After Sorting", grades); 
+            #endregion
+            #region Q4
+            //Console.WriteLine(grades.Find(p=>p>90));
+            #endregion
+            #region Q5
+            //List<int> falingGrades = grades.FindAll(p => p < 75);
+            //ConsoleHelper.PrintList("Failing Gardes", falingGrades);
+            #endregion
+            #region Q6
+            //grades.RemoveAll(p => p<75);
+            //ConsoleHelper.PrintList("Grades with no Failing ", grades);
+
+            #endregion
+            #region Q7 
+            //Console.WriteLine(grades.Contains(100));
+            #endregion
+            #region Q8
+            //List<string> strings = grades.Select(g => $"Grade X: {g}").ToList();
+            //ConsoleHelper.PrintList("strings", strings);
+            #endregion
+            #endregion
+
         }
     }
 }
